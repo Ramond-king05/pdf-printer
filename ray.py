00dd2ac21c1e6a6a2a32a0deaae0b052c56c8fac
Fasasi Abdulrahman Temitope <106965053+Ramond-king05@users.noreply.github.com>
2022-12-07 10:58:24 +0100

    Update ray.py

--- ray.py
+++ ray.py
@@ -68,7 +68,7 @@ else:
     I'M A MACHINE LEARNING AND ARTIFICIAL INTELLIGIENCE DEVELOPER.
     I CREATED THIS APPLICATION SO AS TO HELP PEOPLE TO BE ABLE TO PRINT OUT CERTIFICATE IN PDF FORMAT.
             ''')
-    image = Image.open("image/my (2).jpg")
+    image = Image.open("my.jpg")
     st.image(image,caption=None, width=490, use_column_width=100, clamp=False, channels="RGB", output_format="auto")
     
     
